50efca170bc1e7d109f3b9a3084dbad0f52cf5ee
Hunter Bourgeois <bahunter@gmail.com>
2022-04-27 23:04:29 +0200

    working on chart.js

--- app/javascript/controllers/radar_controller.js
+++ app/javascript/controllers/radar_controller.js
@@ -13,43 +13,11 @@ export default class extends Controller {
   static targets = [ "output" ]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
-    const ctx = document.getElementById('myChart');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-    // const data = {
+    this.outputTarget.textContent = 'Hello, Stimulus Radar Chart!'
+  }
+
+
+  // const data = {
   //   labels: [
   //     'Eating',
   //     'Drinking',
@@ -95,5 +63,4 @@ export default class extends Controller {
   // };
 
   // const myChart = new Chart(this, config);
-  }
 }
